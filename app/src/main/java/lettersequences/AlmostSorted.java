607467e52ec87aef3b94ab7b7120aff5443567a7
Prabesh Narsingh Kunwar <71028458+prabeshkunwar12@users.noreply.github.com>
2024-04-30 15:41:45 -0300

    almost sorted

---
+++ app/src/main/java/lettersequences/AlmostSorted.java
@@ -0,0 +1,70 @@
+package lettersequences;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AlmostSorted {
+    public static void main(String[] args) {
+        almostSorted(new ArrayList<>(List.of(1, 5, 4, 3, 2, 6)));
+        ;
+    }
+
+    public static void almostSorted(List<Integer> arr) {
+        List<Integer> gt = new ArrayList<>();
+        for (int i = 0; i < arr.size() - 1; i++) {
+            if (arr.get(i) > arr.get(i + 1))
+                gt.add(i);
+        }
+        if (gt.size() == 0) {
+            System.out.println("yes");
+        } else if (gt.size() == 1) {
+            int lpos = gt.get(0);
+            int rpos = lpos + 1;
+            int l = arr.get(lpos);
+            int prev = lpos > 0 ? arr.get(lpos - 1) : -1;
+            int r = arr.get(rpos);
+            int after = rpos == arr.size() - 1 ? Integer.MAX_VALUE : arr.get(rpos + 1);
+            if (l <= after && r >= prev)
+                System.out.printf("yes %nswap %d %d", lpos + 1, rpos + 1);
+            else
+                System.out.println("no");
+        } else if (gt.size() == 2) {
+            int lpos = gt.get(0);
+            int rpos = gt.get(1) + 1;
+            int lprev = lpos > 0 ? arr.get(lpos - 1) : -1;
+            int l = arr.get(lpos);
+            int lafter = arr.get(lpos + 1);
+            int rprev = arr.get(rpos - 1);
+            int r = arr.get(rpos);
+            int rafter = rpos == arr.size() - 1 ? Integer.MAX_VALUE : arr.get(rpos + 1);
+            if (r >= lprev && r <= lafter && l >= rprev && l <= rafter)
+                System.out.printf("yes %nswap %d %d", lpos + 1, rpos + 1);
+            else
+                System.out.println("no");
+        } else {
+            boolean sortable = true;
+            for (int i = 0; i < gt.size() - 1; i++) {
+                if (gt.get(i + 1) - gt.get(i) != 1) {
+                    sortable = false;
+                    break;
+                }
+            }
+            if (sortable) {
+                int lpos = gt.get(0);
+                int rpos = gt.get(gt.size() - 1) + 1;
+                int lprev = lpos > 0 ? arr.get(lpos - 1) : -1;
+                int l = arr.get(lpos);
+                int lafter = arr.get(lpos + 1);
+                int rprev = arr.get(rpos - 1);
+                int r = arr.get(rpos);
+                int rafter = rpos == arr.size() - 1 ? Integer.MAX_VALUE : arr.get(rpos + 1);
+                if (r >= lprev && r <= lafter && l >= rprev && l <= rafter)
+                    System.out.printf("yes %nreversed %d %d", lpos + 1, rpos + 1);
+                else
+                    System.out.println("no");
+            } else {
+                System.out.println("no");
+            }
+        }
+    }
+}
